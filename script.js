307ae1e0d79f79193fc3529287d33e03156445d2
Layne Weniger <lweniger@gmail.com>
2021-08-31 02:09:25 -0500

    none

---
+++ script.js
@@ -0,0 +1,6 @@
+var map = new maplibregl.Map({
+    container: 'map', // container id
+    style: 'https://api.maptiler.com/maps/streets/style.json?key=h6w6Fn2TwRbmpGfT7TWo', // style URL
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
